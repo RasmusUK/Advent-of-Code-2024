c1661813cf7ada980ccb7bbf6935844d728f03ce
RasmusUK <rasmus.kristensen@live.dk>
2024-12-04 17:39:25 +0100

    Setup day 1

---
+++ AdventOfCode2024/AdventOfCode2024/Days/Day1/Day1.cs
@@ -0,0 +1,18 @@
+namespace AdventOfCode2024.Days.Day1;
+
+public class Day1 : Day
+{
+    public Day1(int day) : base(day)
+    {
+    }
+
+    protected override object Part1(List<string> input)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override object Part2(List<string> input)
+    {
+        throw new NotImplementedException();
+    }
+}
